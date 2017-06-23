da5b0d70a5ff8cd499c7dc18237f63256c04cc87
Barış Ayar <barisayarr@gmail.com>
2017-06-23 12:28:51 +0300

    Add files via upload

---
+++ md5cracker.py
@@ -0,0 +1,19 @@
+#-*- coding: utf-8 -*-
+import hashlib
+ 
+print """
+###########################
+#         Md5 Cracker     #
+#      www.hubeybi.com    #
+###########################
+"""
+ 
+md5hash = raw_input("Kırılacak Hash: ")
+wordlist = open("wordlist.txt", "r").readlines()
+ 
+for kelime in wordlist:
+	kelime = kelime.strip()
+	kir = hashlib.md5(kelime).hexdigest()
+	if kir == md5hash:
+		print "MD5 Cracklendi: " + kelime
+		break
